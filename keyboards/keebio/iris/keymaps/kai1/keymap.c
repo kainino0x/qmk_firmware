fb746fec71de4e58a047534291102391a0d00cf7
Kai Ninomiya <kainino1@gmail.com>
2021-06-26 15:57:13 -0700

    add wordleft/wordright and shuffle some stuff

--- keyboards/keebio/iris/keymaps/kai1/keymap.c
+++ keyboards/keebio/iris/keymaps/kai1/keymap.c
@@ -2,7 +2,7 @@
 #include "print.h"
 
 #define _OS_LAYOUT( \
-      idx, nextdflt,  kc_cmd,         kc_foo,         cmd,            foo_T,          redo,           delword,        back,           forward) \
+      idx, nextdflt,  kc_cmd,         kc_foo,         cmd,            foo_T,          redo,           word,           back,           forward) \
   [idx] = LAYOUT(                                                                                                                                                                                                                     \
       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,        KC_1,           \
       foo_T(KC_GRV),  KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_LBRC,        \
@@ -11,14 +11,14 @@
                                                                       MO((idx) + 1),  KC_LSFT,        KC_BSPC,        KC_SPC,         KC_LALT,        MO((idx) + 1)),                                                                 \
   [(idx) + 1] = LAYOUT(                                                                                                                                                                                                               \
       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                                          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         \
-      kc_foo,         KC_MPRV,        KC_MNXT,        back,           forward,        redo,                                           KC_PGUP,        KC_HOME,        KC_UP,          KC_END,         XXXXXXX,        KC_BSLS,        \
-      KC_ESC,         cmd(KC_A),      XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,                                        KC_PGDN,        KC_LEFT,        KC_DOWN,        KC_RGHT,        XXXXXXX,        KC_EQL,         \
-      KC_TRNS,        cmd(KC_SLSH),   cmd(KC_B),      cmd(KC_I),      cmd(KC_DOT),    delword,        DF(nextdflt),   DEBUG,          XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_INS,         \
-                                                                      KC_TRNS,        KC_TRNS,        KC_DEL,         KC_TAB,         KC_TRNS,        KC_TRNS)
+      kc_foo,         XXXXXXX,        back,           KC_PGUP,        forward,        redo,                                           KC_MNXT,        KC_HOME,        KC_UP,          KC_END,         XXXXXXX,        KC_BSLS,        \
+      KC_ESC,         cmd(KC_A),      word(KC_LEFT),  KC_PGDN,        word(KC_RGHT),  XXXXXXX,                                        KC_MPRV,        KC_LEFT,        KC_DOWN,        KC_RGHT,        XXXXXXX,        KC_EQL,         \
+      KC_TRNS,        cmd(KC_SLSH),   cmd(KC_B),      cmd(KC_I),      cmd(KC_DOT),    KC_DEL,         DF(nextdflt),   DEBUG,          XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_INS,         \
+                                                                      KC_TRNS,        KC_TRNS,        word(KC_BSPC),  KC_TAB,         KC_TRNS,        KC_TRNS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  _OS_LAYOUT(0, 2,         KC_LCTL,      KC_LGUI,      LCTL,         LGUI_T,        LCTL(KC_T),    LCTL(KC_BSPC), LALT(KC_LEFT), LALT(KC_RIGHT)), // Win
-  _OS_LAYOUT(2, 0,         KC_LGUI,      KC_LCTL,      LGUI,         LGUI_T,        SGUI(KC_SLSH), LALT(KC_BSPC), LGUI(KC_LEFT), LGUI(KC_RIGHT)), // Mac
+  _OS_LAYOUT(0, 2,         KC_LCTL,      KC_LGUI,      LCTL,         LGUI_T,        LCTL(KC_T),       LCTL,           LALT(KC_LEFT),  LALT(KC_RIGHT)), // Win
+  _OS_LAYOUT(2, 0,         KC_LGUI,      KC_LCTL,      LGUI,         LGUI_T,        SGUI(KC_SLSH),    LALT,           LGUI(KC_LEFT),  LGUI(KC_RIGHT)), // Mac
 };
 
 void _update_rgblight_for_layer(uint32_t state) {
